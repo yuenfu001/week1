4f28ed705bd608db83d89ccb7672c0c1a5e614a4
liu yuenfu <liuyuenfu@gmail.com>
2021-11-04 15:47:20 +0100

    new update

--- week_1_project.py
+++ week_1_project.py
@@ -1,21 +1,34 @@
+# The student to be evaluated
 BBcyber = float(input("Enter BBcyber's scores here: "))
-Total_mark = int(input(" Enter the total scores of the course here: "))
-class_score = []
 
+# The total mark of the Course
+Total_mark = int(input("Enter the total scores of the course here: "))
+
+# a python list to take all the student scores taking the course
+class_score_list = []
+
+# This will take the number of students taking the course excluding the evaluated student
 student_num = int(input("enter number of other students in class excluding BBcyber here: "))
 
-
+# a loop to check the student_num starting from index number 1 then add 1 to the student_num
 for i in range(1,student_num+1):
+    #This is take in the scores of the rest of the students taking the course
     student = float(input("Student_score {}: ".format(i)))
-    class_score.append(round((student/Total_mark)*100,0))
+    # this takes in the students score excluding the Evaluated student, convert it into percentage and insert them in the class_score_list
+    class_score_list.append(round((student/Total_mark)*100,0))
 
-class_score.append(BBcyber)
-avg_score = round((sum(class_score)/len(class_score)),0)
+#the appends the evaluated student to the list
+class_score_list.append(BBcyber)
+# the avg_score will take the average of all the students in the updated class_score_list
+avg_score = round((sum(class_score_list)/len(class_score_list)),0)
 
-print(class_score)
+print(class_score_list)
 print("The average score of the class is :",avg_score,"%")
-if BBcyber >= (avg_score-5):
+
+#This is a condition that if the evaluated student is 5 points less than the average score and if he has 60 points
+#meaning if both conditions are true then it is a pass else fail
+if BBcyber >= (avg_score-5) and BBcyber >= 60:
     print("passed")
 
 else:
-    print("E b for you BBcyber")
+    print("Olodo")
