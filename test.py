352938fbf332c74df9d968ec59294643072530b0
liu yuenfu <liuyuenfu@gmail.com>
2021-11-05 22:58:42 +0100

    updated code

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-n = int(input("Enter Total Class here: "))
+Total_class = int(input("Enter Total Number in Class here: "))
 total_mark = int(input("Enter Total mark of course here: "))
 # d=dict(input("Enter name and score with space between them").split(" ") for x in range(n))
 dict1 ={}
-for a in range(1,n+1):
-    keys = input("Enter  Student {}'s name here: ".format(a))
-    values = float(input("Enter Student {}'s scores here: ".format(a)))
+for a in range(1,Total_class+1):
+    keys = input("Enter  Student {}'s name here: \n".format(a))
+    values = float(input("Enter Student {}'s scores here: \n".format(a)))
     dict1[keys] = values
 percent = [(x/total_mark)*100 for x in dict1.values()]
 print(percent)
+print()
 avg_scores = sum(percent)/len(percent)
 
 print("Average score is : ", round(avg_scores,1))
+print()
 print ("{:<25} {:<10} {:<10}(%) {:<10}".format("Name","Score", "Percentage","Comments"))
 for i in range(0, len(dict1.items())):
     p = percent[i]
